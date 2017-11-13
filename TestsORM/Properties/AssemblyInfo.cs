238c30d8af15e6593b7cc7110b9d4aa7e492dad0
Lucas Bouland <lucas.bouland@ynov.com>
2017-11-13 13:11:19 +0100

    CORE->Framework pour les classes ADO

--- TestsORM/Properties/AssemblyInfo.cs
+++ TestsORM/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("nORMeTests")]
+[assembly: AssemblyTitle("TestsORM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nORMeTests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestsORM")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("8351ad57-9a9c-4d3a-a4c4-0bd937f69cc5")]
+[assembly: Guid("13c507b8-32e8-4054-850a-b973b73b894d")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- TestsORM/UnitTest1.cs
+++ TestsORM/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace nORMeTests
+namespace TestsORM
 {
     [TestClass]
     public class UnitTest1
