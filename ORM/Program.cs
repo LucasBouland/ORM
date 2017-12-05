2ed1a2c857ac3647d6710a74b67238328b7e6767
Lucas Bouland <lucas.bouland@ynov.com>
2017-12-06 00:59:32 +0100

    Ajout de la connexion à la base de donnée MySQL + exemple de commande simple en dur à modifier

--- ORM/Program.cs
+++ ORM/Program.cs
@@ -3,13 +3,158 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using Npgsql;
 
 namespace ORM
 {
-    class Program
+
+    // TODO : Rassembler les trois classes en une gestion unique du type de base
+
+    #region Mysql
+    public class DBConnect
     {
-        static void Main(string[] args)
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnect()
         {
+            Initialize();
+        }
+        // TODO : Faire passer les champs via un fichier xml type settings.xml
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "testbdd";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        // TODO : expliciter la gestion des erreurs
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        
+        public void Insert()
+        {
+            string query = "INSERT INTO users (name, age) VALUES ('joey', '18'), ('Jamel', '45')";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+            else
+            {
+                Console.Write("lmao");
+            }
+        }
+        
+        public void Update()
+        {
+            string query = "UPDATE users SET name='Joe', age='22' WHERE name='joey'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = query;
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+        
+        public void Delete()
+        {
+            string query = "DELETE FROM users WHERE name='Joe'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        public List<string> SelectOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM users";
+
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["idusers"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+                dataReader.Close();
+                this.CloseConnection();
+                return list;
+            }
+            else
+            {
+                return list;
+            }
         }
     }
+    #endregion
+
 }
